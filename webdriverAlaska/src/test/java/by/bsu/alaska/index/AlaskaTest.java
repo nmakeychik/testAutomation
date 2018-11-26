966c6cb04e345d97ca3b973d06626d1a588d3f33
Nikita Makeychik <nmakeychik1@gmail.com>
2018-11-26 21:09:25 +0300

    webdriver task

---
+++ webdriverAlaska/src/test/java/by/bsu/alaska/index/AlaskaTest.java
@@ -0,0 +1,40 @@
+package by.bsu.alaska.index;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AlaskaTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setupTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36");
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void isHideReturningDateChoiceAfterOneWayChoice() {
+        driver.get("https://www.alaskaair.com/");
+        driver.findElement(By.xpath("//*[@id=\"oneWay\"]")).click();
+        WebElement returnDateButton = driver.findElement(By.xpath("//*[@id=\"returnDate\"]"));
+        Assert.assertFalse(returnDateButton.isDisplayed());
+    }
+}
